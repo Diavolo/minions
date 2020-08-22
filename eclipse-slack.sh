d44095a38f66d35fcf549c0ee9d5be9aeeaa3ca4
Gustavo Huarcaya <diavolo@gahd.net>
2020-08-22 17:57:25 -0500

    Add eclipse-slack.sh

---
+++ eclipse-slack.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+#
+#  ██████╗  █████╗ ██╗  ██╗██████╗    ███╗   ██╗███████╗████████╗
+# ██╔════╝ ██╔══██╗██║  ██║██╔══██╗   ████╗  ██║██╔════╝╚══██╔══╝
+# ██║  ███╗███████║███████║██║  ██║   ██╔██╗ ██║█████╗     ██║
+# ██║   ██║██╔══██║██╔══██║██║  ██║   ██║╚██╗██║██╔══╝     ██║
+# ╚██████╔╝██║  ██║██║  ██║██████╔╝██╗██║ ╚████║███████╗   ██║
+#  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝
+#   ____  ___  _   _ ____
+#  / ___|/ _ \| | | |  _ \   minions/eclipse-slack.sh
+# | |  _| |_| | |_| | | | |
+# | |_| |  _  |  _  | |_| |  Gustavo Huarcaya
+#  \____|_| |_|_| |_|____/   https://gahd.net
+#
+# Changes old eclipse config file after upgrade.
+#
+
+if [[ -f "/opt/eclipse-jee/configuration/config.ini.new" ]]; then
+    cd /opt/eclipse-jee/configuration/
+    mv config.ini.new config.ini
+    echo "config.ini.new file renamed"
+fi
+
+if [[ -f "/opt/eclipse-jee/eclipse.ini.new" ]]; then
+    cd /opt/eclipse-jee/
+    mv eclipse.ini.new eclipse.ini
+    echo "eclipse.ini.new file renamed"
+fi
