693656d8888b803eddbce5bffe4720b1f013f006
Gustavo Huarcaya <diavolo@gahd.net>
2025-08-03 12:11:48 -0500

    chore: rename file

--- bash/slack-eclipse.sh
+++ bash/slack-eclipse.sh
@@ -7,14 +7,22 @@
 # ╚██████╔╝██║  ██║██║  ██║██████╔╝██╗██║ ╚████║███████╗   ██║
 #  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝
 #   ____  ___  _   _ ____
-#  / ___|/ _ \| | | |  _ \   minions/bash/kernel-slack.sh
+#  / ___|/ _ \| | | |  _ \   minions/bash/slack-eclipse.sh
 # | |  _| |_| | |_| | | | |
 # | |_| |  _  |  _  | |_| |  Gustavo Huarcaya
 #  \____|_| |_|_| |_|____/   https://gahd.net
 #
-# Update elilo config after kernel upgrade.
+# Change old eclipse config files after upgrade.
 #
 
-geninitrd
-eliloconfig
+if [[ -f "/opt/eclipse-jee/configuration/config.ini.new" ]]; then
+    cd /opt/eclipse-jee/configuration/
+    mv config.ini.new config.ini
+    echo "config.ini.new file renamed"
+fi
 
+if [[ -f "/opt/eclipse-jee/eclipse.ini.new" ]]; then
+    cd /opt/eclipse-jee/
+    mv eclipse.ini.new eclipse.ini
+    echo "eclipse.ini.new file renamed"
+fi
--- bash/slack-kernel.sh
+++ bash/slack-kernel.sh
@@ -7,22 +7,13 @@
 # ╚██████╔╝██║  ██║██║  ██║██████╔╝██╗██║ ╚████║███████╗   ██║
 #  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝
 #   ____  ___  _   _ ____
-#  / ___|/ _ \| | | |  _ \   minions/bash/eclipse-slack.sh
+#  / ___|/ _ \| | | |  _ \   minions/bash/slack-kernel.sh
 # | |  _| |_| | |_| | | | |
 # | |_| |  _  |  _  | |_| |  Gustavo Huarcaya
 #  \____|_| |_|_| |_|____/   https://gahd.net
 #
-# Change old eclipse config files after upgrade.
+# Update elilo config after kernel upgrade.
 #
 
-if [[ -f "/opt/eclipse-jee/configuration/config.ini.new" ]]; then
-    cd /opt/eclipse-jee/configuration/
-    mv config.ini.new config.ini
-    echo "config.ini.new file renamed"
-fi
-
-if [[ -f "/opt/eclipse-jee/eclipse.ini.new" ]]; then
-    cd /opt/eclipse-jee/
-    mv eclipse.ini.new eclipse.ini
-    echo "eclipse.ini.new file renamed"
-fi
+geninitrd
+eliloconfig
